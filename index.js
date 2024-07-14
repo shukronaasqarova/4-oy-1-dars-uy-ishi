e5211d97f914a7fb13719a06eb6963ec7739b9b0
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-07-14 22:58:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,143 @@
+// let num1 = +prompt("son");
+// let num = +prompt("son");
+
+// console.log(num1 + num);
+
+
+
+
+
+
+
+
+
+
+// osonroq masalalar
+
+// 1 masala
+
+// let a = +prompt("son kirirting");
+// let b = Math.trunc(a/2);
+// let c = a % 2;
+// console.log("butun son", b, "qoldiq son", c);
+
+// 2 masala
+
+// let a = +prompt("1-sonni kirirting");
+// let b = +prompt("2-sonni kirirting");
+// if (a > b){
+//     console.log(a * b);
+// } else if(a = b) {
+//     console.log(a - b);
+// } else{
+//     console.log(a + b);
+// }
+
+// 3 masala xatoo
+
+// let a = +prompt ("son kiriting");
+// let b = !a;
+// console.log(b);
+
+// 7 masala
+
+// let a = +prompt("a ni kiriting");
+// let res = Math.trunc(a*5);
+// console.log(res);
+
+// INTEGER
+
+// integer 1
+
+// let l = +prompt("L ni kiriting");
+// let res = Math.trunc(l / 100);
+// console.log(res);
+
+// integer 2
+
+// let m = +prompt("M ni kiriting");
+// let res = Math.trunc(m / 1000);
+// console.log(res);
+
+// integer 3
+
+// let kb = +prompt("kb ni kiriting");
+// let res = Math.trunc(kb / 1024);
+// console.log(res);
+
+// integer 4
+
+// let a = +prompt ('a');
+// let b = +prompt ('b');
+// let result = Math.trunc(a/b);
+// console.log(result);
+
+// integer 5
+
+// let a = +prompt ('a');
+// let b = +prompt ('b');
+// let result = Math.trunc(a % b);
+// console.log(result);
+
+// integer 6
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// console.log (b,o );
+
+// integer 7
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// let res = Math.trunc(b + o);
+// console.log(res);
+
+// integer 8
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// let res = Math.trunc(b*10 + o);
+// console.log(res);
+
+// integer 9
+
+// let n = +prompt ('n sonini kiriting');
+// let y = Math.trunc((n/100)%100);
+// console.log(y);
+
+// integer 10
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// console.log (b,o );
+
+// integer 11
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// let y = Math.trunc((n/100)%100);
+// let res =Math.trunc(b+o+y)
+// console.log(res);
+
+// integer 12
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// let y = Math.trunc((n/100)%100);
+// let res = Math.trunc(b*100+o*10+y);
+// console.log(res);
+
+// integer 13
+
+// let n = +prompt ('n sonini kiriting');
+// let b = Math.trunc (n%10);
+// let o = Math.trunc ((n/10)%10);
+// let y = Math.trunc((n/100)%100);
+// let res = Math.trunc(y*100+b*10+y);
+// console.log(res);
